944ba05d4a805a1f0b107d17b12cde689a779fd1
Vipin Yadav <vipinyadav546@gmail.com>
2020-07-30 23:26:01 +0530

    infix to postfix

--- stack.cpp
+++ stack.cpp
@@ -18,9 +18,52 @@ int isBalanced(char *expre){
     else return 0;
     
 }
+int isOperand(char x){
+    if(x=='+'|| x=='-' || x=='*' || x=='/')
+    return 0;
+    else 
+    return 1;
+}
+
+int pre(char x){
+    if(x=='+'|| x=='-')
+        return 1;
+    else if (x=='*'|| x=='/') return 2;
+    else return 0;
+}
+char* infixtopostfix(char *expre){
+    stack <char> infix;
+    int i=0,j=0;
+    char *postfix=new char[infix.size()+1];
+
+    while(expre[i]!='\0'){
+        if(isOperand(expre[i]))
+            postfix[j++]=expre[i++];
+        else{
+            if(!infix.empty()){
+            if(pre(expre[i]>pre(infix.top())))
+            infix.push(expre[i++]);
+            else
+            {
+                postfix[j++]=infix.top();
+                infix.pop();
+            }
+            
+            }
+            else infix.push(expre[i++]);
+        }
+    }
+    while(!infix.empty()) {
+        postfix[j++]=infix.top();
+        infix.pop();
+}
+postfix[j]='\0'; 
+return postfix;
+}
 int main(){
 
-    char *expre="(a*b)+)b+a)";
-    cout<<isBalanced(expre)<<" ** ";
-
+    char *expre="a+b";
+    // cout<<isBalanced(expre)<<" ** ";
+    cout<<infixtopostfix(expre);
+    
     }
