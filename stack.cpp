ee6c16b2dbac904995b178d50a39a7706b526c7b
Vipin Yadav <vipinyadav546@gmail.com>
2020-08-01 23:35:20 +0530

    inifx to postfix

--- stack.cpp
+++ stack.cpp
@@ -19,9 +19,10 @@ int isBalanced(char *expre){
     
 }
 int isOperand(char x){
-    if(x=='+'|| x=='-' || x=='*' || x=='/')
-    return 0;
-    else 
+    if (x == '+' || x == '-' || x == '*' || x == '/' ||
+        x == '^' || x == '(' || x == ')'){
+        return 0;
+    }
     return 1;
 }
 
@@ -60,10 +61,69 @@ char* infixtopostfix(char *expre){
 postfix[j]='\0'; 
 return postfix;
 }
+
+int outPrecedence(char x){
+    if (x == '+' || x == '-'){
+        return 1;
+    } else if (x == '*' || x == '/'){
+        return 3;
+    } else if (x == '^'){
+        return 6;
+    } else if (x == '('){
+        return 7;
+    } else if (x == ')'){
+        return 0;
+    }
+    return -1;
+}
+
+int inPrecedence(char x){
+    if (x == '+' || x == '-'){
+        return 2;
+    } else if (x == '*' || x == '/'){
+        return 4;
+    } else if (x == '^'){
+        return 5;
+    } else if (x == '('){
+        return 0;
+    }
+    return -1;
+}
+
+char *convert(char* infix){
+    stack <char> stk;
+    char* postfix=new char[strlen(infix)+1];
+    int i=0,j=0;
+    while(infix[i]!='\0'){
+        if(isOperand(infix[i])){
+            postfix[j++]=infix[i++];
+        }
+        else{
+            if(stk.empty()|| outPrecedence(infix[i])> inPrecedence(stk.top())){
+                stk.push(infix[i++]);
+            }else if(outPrecedence(infix[i]) == inPrecedence(stk.top())){
+                    stk.pop();
+            }
+            else{
+                postfix[j++]=stk.top();
+                stk.pop();
+            }
+        }
+    }
+    while(!stk.empty() && stk.top()!=')'){
+        postfix[j++] = stk.top();
+        stk.pop();
+    }
+    postfix[j]='\0';
+    return postfix;
+}
+
 int main(){
 
-    char *expre="a+b";
+    char expre[]="((a+b)*c)-d^e^f";
     // cout<<isBalanced(expre)<<" ** ";
-    cout<<infixtopostfix(expre);
+    //cout<<infixtopostfix(expre);
+    cout<<convert(expre)<<endl;
     
     }
+
